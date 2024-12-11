950446ff5a7d277d70d573020f594cb00d5ec6e2
Armaan <imaap143@gmail.com>
2024-12-11 22:19:03 +0530

    Commit initial project'

---
+++ src/test/java/org/productcatalog/ecommerceproductcatalog/EcommerceProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.productcatalog.ecommerceproductcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
